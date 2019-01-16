c2cb9fc7e5c8bfb60be5aa2ca72aa432725f63b9
Longxiang Li <longxiang1025@gmail.com>
2019-01-16 14:45:20 -0500

    we should fix the corGaus parameters

--- code/11_pb_mixed_effect_model.R
+++ code/11_pb_mixed_effect_model.R
@@ -57,8 +57,7 @@ options(dplyr.print_max = 1e9)
 #' this circle in the study period, this RadNet monitor is categorized as within gas/oil field.
 #' Otherwise, this RadNet monitor is categorized as clean ones.
 #+ Loading the Lead-210 and gas/oil production data within 25km, message=F, echo=F,warning=F 
-radius=50
-load(here::here("data",paste0("pb_gas_oil_",radius,".RData")))
+load(here::here("data","pb_gas_oil_50.RData"))
 ##############################################
 #Massage the pb-210 dataset
 rad_cross[is.na(rad_cross$Radon),]$Radon=6
@@ -73,6 +72,99 @@ rad_cross$Play<-rad_cross$Oil_Field|rad_cross$Gas_Field
 rad_cross[,grep("Prod",names(rad_cross))]=rad_cross[,grep("Prod",names(rad_cross))]/1e6
 rad_cross[,grep("Num",names(rad_cross))]=rad_cross[,grep("Num",names(rad_cross))]/1e3
 ###################################################
+rad_cross$pb210<-rad_cross$pb210*1000
+
+##################
+#Descriptive Statistics
+play_data<-rad_cross[rad_cross$Play,]
+out_data<-rad_cross[!rad_cross$Play,]
+write_row<-function(play_dat,out_dat,name)
+{
+  t0<-t.test((rbind(play_dat,out_dat))[[name]])
+  p0<-c(t0$estimate,t0$conf.int[1:2])
+  t1<-t.test(play_dat[[name]])
+  p1<-c(t1$estimate,t1$conf.int[1:2])
+  t2<-t.test(out_dat[[name]])
+  p2<-c(t2$estimate,t2$conf.int[1:2])
+  p3<-(t.test(play_dat[[name]],out_dat[[name]]))$p.value
+  return(as.numeric(c(p0,p1,p2,p3)))
+}
+
+write_cor_row<-function(play_dat,out_dat,name1,name2){
+  c0<-cor.test((rbind(play_dat,out_dat))[[name1]],(rbind(play_dat,out_dat))[[name2]],conf.level=0.95)
+  t<-paired.r(cor(play_dat[[name1]],play_dat[[name2]],use="complete.obs"),
+              cor(out_dat[[name1]],out_dat[[name2]],use="complete.obs"),
+           n=nrow(play_dat), n2=nrow(out_dat),twotailed=TRUE)
+  c1<-cor.test(play_dat[[name1]],play_dat[[name2]],conf.level=0.95)
+  c2<-cor.test(out_dat[[name1]],out_dat[[name2]],conf.level=0.95)
+  return(as.numeric(c(c0$estimate,c0$conf.int[1:2],c1$estimate,c1$conf.int[1:2],c2$estimate,c2$conf.int[1:2],t$p)))
+}
+
+pb_row<-write_row(play_data,out_data,"pb210")
+pb_Rn_row<-write_row(play_data,out_data,"Radon")
+pb_mass_row<-write_row(play_data,out_data,"mass")
+pb_U_row<-write_row(play_data,out_data,"Umeans")
+pb_vel_row<-write_row(play_data,out_data,"vel")
+pb_hpbl_row<-write_row(play_data,out_data,"hpbl")
+pb_cstdist_row<-write_row(play_data,out_data,"Coast_Dist")
+pb_oil_row<-write_row(play_data,out_data,"G_Oil_Prod")
+pb_gas_row<-write_row(play_data,out_data,"G_Gas_Prod")
+
+c_pb_radon_row<-write_cor_row(play_data,out_data,"pb210","Radon")
+c_pb_U_row<-write_cor_row(play_data,out_data,"pb210","Umeans")
+c_pb_mass_row<-write_cor_row(play_data,out_data,"pb210","mass")
+c_pb_vel_row<-write_cor_row(play_data,out_data,"pb210","vel")
+c_pb_hpbl_row<-write_cor_row(play_data,out_data,"pb210","hpbl")
+c_pb_cstdist_row<-write_cor_row(play_data,out_data,"pb210","Coast_Dist")
+c_pb_oil_row<-write_cor_row(play_data,out_data,"pb210","G_Oil_Prod")
+c_pb_gas_row<-write_cor_row(play_data,out_data,"pb210","G_Gas_Prod")
+
+beta_row<-write_row(play_beta_data,out_beta_data,"beta")
+beta_Rn_row<-write_row(play_beta_data,out_beta_data,"Radon")
+beta_mass_row<-write_row(play_beta_data,out_beta_data,"mass")
+beta_U_row<-write_row(play_beta_data,out_beta_data,"Umeans")
+beta_vel_row<-write_row(play_beta_data,out_beta_data,"vel")
+beta_hpbl_row<-write_row(play_beta_data,out_beta_data,"hpbl")
+beta_cstdist_row<-write_row(play_beta_data,out_beta_data,"Coast_Dist")
+beta_oil_row<-write_row(play_beta_data,out_beta_data,"G_Oil_Prod")
+beta_gas_row<-write_row(play_beta_data,out_beta_data,"G_Gas_Prod")
+
+c_beta_pb_row<-write_cor_row(play_beta_data,out_beta_data,"beta","pb210")
+c_beta_radon_row<-write_cor_row(play_beta_data,out_beta_data,"beta","Radon")
+c_beta_U_row<-write_cor_row(play_beta_data,out_beta_data,"beta","Umeans")
+c_beta_mass_row<-write_cor_row(play_beta_data,out_beta_data,"beta","mass")
+c_beta_vel_row<-write_cor_row(play_beta_data,out_beta_data,"beta","vel")
+c_beta_hpbl_row<-write_cor_row(play_beta_data,out_beta_data,"beta","hpbl")
+c_beta_cstdist_row<-write_cor_row(play_beta_data,out_beta_data,"beta","Coast_Dist")
+c_beta_oil_row<-write_cor_row(play_beta_data,out_beta_data,"beta","G_Oil_Prod")
+c_beta_gas_row<-write_cor_row(play_beta_data,out_beta_data,"beta","G_Gas_Prod")
+
+table_2<-rbind.data.frame(pb_row,pb_Rn_row,pb_mass_row,pb_U_row,pb_vel_row,pb_hpbl_row,pb_cstdist_row,
+                          pb_oil_row,pb_gas_row,
+                          c_pb_radon_row,c_pb_mass_row,c_pb_U_row,c_pb_vel_row,c_pb_hpbl_row,c_pb_cstdist_row,
+                          c_pb_oil_row,c_pb_gas_row)
+names(table_2)<-c("g_mean","gross_low","g_high","p_mean","p_low","p_high","o_mean",
+                  "o_low","o_high","sig_dif")
+row.names(table_2)<-c("Pb-210","Radon Index","PM2.5","U-238","Wind velocity","HPBL","Dist to Coast",
+                      "Gross Oil Production","Gross Gas Production",
+                      "Pb-210 & Radon","Pb-210 & PM2.5","Pb-210 & U238","Pb-210 & Wind velocity","Pb-210 & HPBL","PB-210 & Dist to Coast",
+                      "Pb-210 & Oil Prod","Pb-210 & Gas Prod"
+                      )
+
+table_3<-rbind.data.frame(beta_row,beta_Rn_row,beta_mass_row,beta_U_row,beta_vel_row,beta_hpbl_row,beta_cstdist_row,
+                          beta_oil_row,beta_gas_row,
+                          c_beta_radon_row,c_beta_mass_row,c_beta_U_row,c_beta_vel_row,c_beta_hpbl_row,c_beta_cstdist_row,
+                          c_beta_oil_row,c_beta_gas_row)
+names(table_3)<-c("g_mean","gross_low","g_high","p_mean","p_low","p_high","o_mean",
+                  "o_low","o_high","sig_dif")
+row.names(table_3)<-c("beta","Radon Index","PM2.5","U-238","Wind velocity","HPBL","Dist to Coast",
+                      "Gross Oil Production","Gross Gas Production",
+                      "beta & Radon","beta & PM2.5","beta & U238","beta & Wind velocity","beta & HPBL","beta & Dist to Coast",
+                      "beta & Oil Prod","beta & Gas Prod"
+)
+library(xtable)
+xtable(table_2,digits = c(5,2,2,2,2,2,2,2,2,2,3))
+xtable(table_3,digits = c(5,2,2,2,2,2,2,2,2,2,3))
 #################
 #Models
 rad_cross[is.na(rad_cross$mass),]$mass<-mean(rad_cross$mass,na.rm=T)
@@ -149,17 +241,8 @@ sens_range<-sens_result%>%group_by(metric)%>%summarise(max_gam=max(gam_coef),
                                                        max_sar=max(sar_coef),
                                                        min_sar=min(sar_coef))
 result<-left_join(result,sens_range)
-addtorow <- list()
-addtorow$pos <- list(0, 0, 0)
-addtorow$command <-c("& \\multicolumn{6}{c}{GAM}&\\multicolumn{6}{c}{GLS}&\\multicolumn{6}{c}{SAR} \\\\\n",
-                     "\\hline",
-                     "Metric & Slope & P-value & 2.5\\%CI & 97.5\\%CI&Sens\\_Low&Sens\\_Up& Slope & P-value & 2.5\\%CI & 97.5\\%CI&Sens\\_Low&Sens\\_Up& Slope & P-value & 2.5\\%CI & 97.5\\%CI&Sens\\_Low&Sens\\_Up \\\\\n")
-table=xtable::xtable(result[,c(13,1,4,2,3,15,14,5,8,6,7,17,16,9,12,10,11,19,18)],caption=paste0("summary of three ways to adjust for spatial confounding (",radius ,"km case)"),digits=4)
-xtable::align(table)<-"|ll|rrrrrr|rrrrrr|rrrrrr|"
-print(table, floating = TRUE, floating.environment = "sidewaystable",include.rownames=F,scalebox = 0.85,add.to.row = addtorow,include.colnames=F,table.placement="htpb")
-#(result[result$gam_P<0.05,grepl("gam",names(result))])
+(result[result$gam_P<0.05,grepl("gam",names(result))])
 result%>%filter(gam_P<0.05,gls_P<0.05,sar_P<0.05)%>%select(gam_coef,gls_coef,sar_coef,metric)
-save(file = here::here("result",paste0("Model_1_",radius,".RData")),result)
-#print(result)
+print(result)
 
 
--- code/19_Model_Summary.R
+++ code/19_Model_Summary.R
@@ -1,4 +1,8 @@
 library(dplyr)
+library(mgcv)
+library(lme4)
+library(spdep)
+library(xtable)
 files<-list.files(here::here("result"))
 result<-list()
 sp_sens<-list()
@@ -58,3 +62,86 @@ addtorow$command <-c("& \\multicolumn{4}{c}{50km}&\\multicolumn{4}{c}{75km}&\\mu
 table2<-xtable(table2,digits = 4)
 print(table1,include.rownames=F)
 print(table2,add.to.row = addtorow,include.colnames=F)
+#################################################
+#cross-sectional model summary table
+results<-list()
+j=0
+for(r in c(50,75,100)){
+  j=j+1
+  load(here::here("data",paste0("pb_gas_oil_",r,".RData")))
+  ##############################################
+  #Massage the pb-210 dataset
+  rad_cross[is.na(rad_cross$Radon),]$Radon=6
+  rad_cross$basin<-as.factor(rad_cross$basin)
+  rad_cross$log_dist<-log(rad_cross$Coast_Dist)
+  rad_cross$log_pb<-log(rad_cross$pb210)
+  
+  rad_cross$Oil_Field<-(rad_cross$G_Oil_Num>0)
+  rad_cross$Gas_Field<-(rad_cross$G_Gas_Num>0)
+  rad_cross$Play<-rad_cross$Oil_Field|rad_cross$Gas_Field
+  
+  rad_cross[,grep("Prod",names(rad_cross))]=rad_cross[,grep("Prod",names(rad_cross))]/1e6
+  rad_cross[,grep("Num",names(rad_cross))]=rad_cross[,grep("Num",names(rad_cross))]/1e3
+  rad_cross$pb210<-rad_cross$pb210*1000
+  rad_cross[is.na(rad_cross$mass),]$mass<-mean(rad_cross$mass,na.rm=T)
+  rad_cross[is.na(rad_cross$vel),]$vel<-mean(rad_cross$vel,na.rm=T)
+  rad_cross$ppm<-rad_cross$pb210/rad_cross$mass
+  coords <- as.matrix(rad_cross[,c("Lon","Lat")])
+  col.knn <- knearneigh(coords, k=7)
+  W_dist<-dnearneigh(coords,0,1500,longlat = T)
+  W_dist<-nb2listw(W_dist, glist=NULL, style="W", zero.policy=NULL)
+  
+  variables<-names(rad_cross)[c(9:11,13:45)]
+  result<-matrix(0,ncol=12,nrow=length(variables))
+  for(i in 1:length(variables)){
+    var=variables[i]
+    rad_cross[is.na(rad_cross[,var]),var]=0
+    #gam comparison section
+    small_formula="log_pb~Radon+mass+Coast_Dist+Umeans+vel+s(Lon,Lat,k=23)"
+    large_gam_formula=paste0(small_formula,"+",var)
+    g_0<-gam(as.formula(small_formula),data=rad_cross)
+    g_m<-gam(as.formula(large_gam_formula),data=rad_cross)
+    #glm comparison section
+    small_formula="log_pb~Radon+mass+Coast_Dist+Umeans+vel"
+    large_gls_formula=paste0(small_formula,"+",var)
+    g_ls<-gls(as.formula(large_gls_formula),correlation=corGaus(form=~Lon+Lat,nugget=TRUE,fixed = T),data=rad_cross)
+    t<-coef(g_ls)[7]/sqrt(diag(vcov(g_ls)))[7]
+    #summary(g_ls)
+    sar_m<-lagsarlm(as.formula(large_gls_formula),data=rad_cross,listw =  W_dist)
+    t2<-coef(sar_m)[8]/sqrt(diag(vcov(sar_m)))[8]
+    #summary(sar_m)
+    result[i,]<-c(coef(g_m)[7],
+                  sqrt(diag(vcov.gam(g_m)))[7],
+                  2*pnorm(-abs(coef(g_m)[7]/sqrt(diag(vcov.gam(g_m)))[7])),
+                  sum(residuals(g_m)^2),
+                  coef(g_ls)[7],
+                  sqrt(diag(vcov(g_ls)))[7],
+                  2*pnorm(-abs(coef(g_ls)[7]/sqrt(diag(vcov(g_ls)))[7])),
+                  sum(residuals(g_ls)^2),
+                  coef(sar_m)[8],
+                  sqrt(diag(vcov(sar_m)))[8],
+                  2*pnorm(-abs(coef(sar_m)[8]/sqrt(diag(vcov(sar_m)))[8])),
+                  sum(residuals(sar_m)^2)
+    )
+  }
+  result<-as.data.frame(result)
+  names(result)<-c("gam_coef","gam_sd","gam_p","gam_res","gls_coef","gls_sd","gls_p","gls_res","sar_coef","sar_sd","sar_p","sar_res")
+  result$metric<-variables
+  result<-result%>%filter(gam_p<0.05,gls_p<0.05,sar_p<0.05)
+  print(result)
+  result$coef<-0
+  for(i in 1:nrow(result)){
+    result[i,]$coef<-weighted.mean(x=c(result[i,]$gam_coef,result[i,]$gls_coef,result[i,]$sar_coef),w=c(1/result[i,]$gam_sd^2,1/result[i,]$gls_sd^2,1/result[i,]$sar_sd^2)) 
+  }
+  names(result)[14]<-paste0("coef_",r)
+  results[[j]]=result
+}
+table<-full_join(results[[1]][,c(13,14)],results[[2]][,c(13,14)])
+table<-full_join(table,results[[3]][,c(13,14)])
+table$Metric<-c("Gross Prod of Oil","Prod Dens of Oil From Unconv Drills","Prod Dens of Oil From All Wells",
+                "Prod of Oil From Unconv Drills","Prod of Oil From Conv Drills","Prod of Oil From All Upwind Wells",
+                "Num of Unconv Drills","Num of All Active Oil Wells","Prod Dens of Oil From Conv Drills","Number of Conv Drills")
+names(table)<-c("Metric","Slope(50km)","Slope(75km)","Slope(100km)","Metric")
+table_cross<-xtable(table[c(4,5,1,7,10,8,2,9,3,6),c(5,2,3,4)],digits = 4,method="compact",column.names=)
+print(table_cross,include.rownames = FALSE,table.placement="htpb")
+
